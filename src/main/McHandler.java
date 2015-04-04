88d3141b92f453e357373f6e4b9c4c7f7485fd65
Cowtard <ei12109@fe.up.pt>
2015-04-04 18:16:38 +0100

    Envio do chunk requisitado (?)
    Quando vier do futebol vejo isto.

--- src/main/McHandler.java
+++ src/main/McHandler.java
@@ -1,7 +1,12 @@
 package main;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.Queue;
+
+import serviceInterfaces.Chunk;
 
 public class McHandler extends Thread {
 		private Queue<String> msgQueue;
@@ -23,8 +28,25 @@ public class McHandler extends Thread {
 					if (isValid && restore) {
 						String filename = new String("CHUNKS" + File.separator + new String(msg[2]) + File.separator + msg[3]);
 						File tmp = new File(filename);
+						long tmpSize = tmp.length();
+						long readLength = 64000;
+						FileInputStream readStream;
+						byte[] byteChunkPart;
+						try {
+							readStream = new FileInputStream(tmp);
+							if (tmpSize < 64000)
+								readLength = tmpSize;
+							byteChunkPart = new byte[(int) readLength];
+							int read = readStream.read(byteChunkPart, 0, (int)readLength);
+							readStream.close();
+							
+							sendChunk( createChunkMessage(byteChunkPart,msg[2],msg[3]));
+						}catch (IOException exception) {
+							exception.printStackTrace();
+						}
 					}
-					else Main.errorsLog.appendLog("Message not properly written");
+					else if (isValid && !restore){}
+					else Main.errorsLog.appendLog("Message not properly written. FILE ID : " + msg[2]);
 				}
 			}
 		}
@@ -51,4 +73,26 @@ public class McHandler extends Thread {
 			return this.numberOfConfirmations;
 		}
 	
+		private void sendChunk(byte[] messageCompleted) throws IOException{
+			long Initial_t, current_t;
+			Initial_t = System.currentTimeMillis();
+			current_t = System.currentTimeMillis();
+			while( current_t < Initial_t + 500)
+				current_t = System.currentTimeMillis();
+			Main.mdr.send(messageCompleted);
+		}
+		
+		private byte[] createChunkMessage(byte[] data, String fileid, String chunkNo) throws IOException{
+			String chunkInformation = "CHUNK 1.0 " + fileid + " " + chunkNo + " ";
+			ByteArrayOutputStream msgStream = new ByteArrayOutputStream();
+			msgStream.write(chunkInformation.getBytes());
+			msgStream.write((byte) 0x0d);
+			msgStream.write((byte) 0x0a);
+			msgStream.write((byte) 0x0d);
+			msgStream.write((byte) 0x0a);
+			msgStream.write(data);
+			byte[] messageCompleted = msgStream.toByteArray();
+			
+			return messageCompleted;
+		}
 }
